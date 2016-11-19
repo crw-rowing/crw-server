1b7b7cc38ab58f156b0cdc2e60d62bc2f8f39199
Ruud Verbeek <ruud@arfie.nl>
2016-11-20 00:29:51 +0100

    PEP8 fixes

--- http_server.py
+++ http_server.py
@@ -24,18 +24,19 @@ class FileServer(BaseHTTPRequestHandler):
     def send_file(self, fname, write=True):
         """
         Reads a file and sends as HTTP response, including the MIME type.
-        write indicates if the file should actually be sent, or only the headers
+        write indicates if the file should actually be sent,
+        or only the headers
         """
         fname = self.resolve_filename(fname)
         if not fname.startswith('static/'):
-            self.send_response(403) # Forbidden
+            self.send_response(403)  # Forbidden
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             self.wfile.write('Forbidden')
         else:
             try:
                 f = open(fname)
-                self.send_response(200) # OK
+                self.send_response(200)  # OK
                 mime = guess_type(fname)[0] or 'text/plain'
                 self.send_header('Content-type', mime)
                 self.end_headers()
@@ -44,16 +45,17 @@ class FileServer(BaseHTTPRequestHandler):
                 f.close()
             except IOError, e:
                 if e.errno == errno.ENOENT:
-                    self.send_response(404) # Not Found
+                    self.send_response(404)  # Not Found
                     self.send_header('Content-type', 'text/html')
                     self.end_headers()
                 else:
-                    self.send_response(403) # Forbidden
+                    self.send_response(403)  # Forbidden
                     self.send_header('Content-type', 'text/html')
                     self.end_headers()
-                
+
                 if write:
-                    self.wfile.write('IOError ({})'.format(errno.errorcode[e.errno]))
+                    self.wfile.write(
+                        'IOError ({})'.format(errno.errorcode[e.errno]))
 
     def do_HEAD(self):
         self.send_file(self.path, write=False)
