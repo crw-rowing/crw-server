944b09b8829725044e1ff5f4250ec838c881195c
Ruud Verbeek <ruud@arfie.nl>
2016-11-19 23:41:00 +0100

    Serves files

--- http_server.py
+++ http_server.py
@@ -1,4 +1,6 @@
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from mimetypes import guess_type
+import errno
 
 
 def serve(host, port):
@@ -8,11 +10,35 @@ def serve(host, port):
 
 
 class FileServer(BaseHTTPRequestHandler):
+    def send_file(self, fname, write=True):
+        """
+        Reads a file and sends as HTTP response, including the MIME type.
+        write indicates if the file should actually be sent, or only the headers
+        """
+        mime = guess_type(fname)[0] or 'text/plain'
+        try:
+            f = open(fname)
+            self.send_response(200) # OK
+            self.send_header('Content-type', mime)
+            self.end_headers()
+            if write:
+                self.wfile.write(f.read())
+            f.close()
+        except IOError, e:
+            if e.errno == errno.ENOENT:
+                self.send_response(404) # Not Found
+                self.send_header('Content-type', mime)
+                self.end_headers()
+            else:
+                self.send_response(500) # Internal Server Error, idk what else
+                self.send_header('Content-type', mime)
+                self.end_headers()
+            
+            if write:
+                self.wfile.write('IOError ({})'.format(errno.errorcode[e.errno]))
+
     def do_HEAD(self):
-        self.send_response(200) # OK
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+        self.send_file('.' + self.path, write=False)
 
     def do_GET(self):
-        self.do_HEAD()
-        self.wfile.write('Hello World!')
+        self.send_file('.' + self.path)
