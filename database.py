f883bbd33c63b4eecbf1874ad29dee91e57a7010
Marien Raat <marienraat@riseup.net>
2016-11-27 22:53:15 +0100

    Raise an error when a duplicate user is added

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 import psycopg2
 
+
 class UserDatabase:
     def __init__(self, database_name, database_user):
         self.database_connection = psycopg2.connect(database=database_name,
-                                             user=database_user)
+                                                    user=database_user)
         self.cursor = self.database_connection.cursor()
 
     def init_database(self):
@@ -21,12 +22,23 @@ class UserDatabase:
         """Adds an user to the database, using the given email as
         email, the given password as password and a new id, one higher
         than the highest id in the database."""
+
+        # Check if there isn't already an user with this email address
         self.cursor.execute(
-            """SELECT id FROM users;""");
+            """SELECT email FROM users
+            WHERE email = %s;""", (email,))
+        if self.cursor.fetchone() is not None:
+            raise ValueError("An user with this email already exists")
+
+        # Find the current max_id to generate a new id that is one
+        # higher
+        self.cursor.execute(
+            """SELECT id FROM users;""")
         ids = self.cursor.fetchall()
         max_id = 0
         for userid in ids:
             max_id = max(userid[0], max_id)
+
         self.cursor.execute(
             """INSERT INTO users (id, email, password) VALUES
             (%s, %s, %s);""", (max_id + 1, email, password))
