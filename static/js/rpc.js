95d7621ad597de0e62e2d634d0f8a010507a4201
Luuk G <Luuk G>
2016-12-12 14:50:57 +0100

    You can now retrieve a sessionkey

--- static/js/rpc.js
+++ static/js/rpc.js
@@ -2,7 +2,7 @@ let i=0;
 function RPCcall(method, params, callback){
     i++;
     let obj = {"jsonrpc": "2.0", "method": method, "params": params, "id": i};
-
+	
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if ( this.readyState == 4 && this.status == 200){
