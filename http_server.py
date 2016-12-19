c094bf7eb045de3d34e295e0895d5cc5eee76f3e
Ruud Verbeek <ruud@arfie.nl>
2016-12-19 15:57:09 +0100

    remove static/ from url, not needed

--- http_server.py
+++ http_server.py
@@ -30,25 +30,7 @@ def serve(host, port, database_name, database_user):
 
 class FileServer(BaseHTTPRequestHandler):
     redirects = {
-        '': 'static/promo/index.html',
-        'favicon.ico': 'static/favicon.ico',
-        'css/bootstrap.min.css' : 'static/promo/css/bootstrap.min.css',
-        'css/landing-page.css' : 'static/promo/css/landing-page.css',
-        'font-awesome/css/font-awesome.min.css' : 'static/promo/font-awesome/css/font-awesome.min.css',
-        'js/jquery.js' : 'static/promo/js/jquery.js',
-        'js/bootstrap.min.js' : 'static/promo/js/bootstrap.min.js',
-        'img/crw_logo_golf.png' : 'static/promo/img/crw_logo_golf.png',
-        'img/ipad.png' : 'static/promo/img/ipad.png',
-        'img/crw_laptop1.png' : 'static/promo/img/crw_laptop1.png',
-        'img/crwlogo-pc.png' : 'static/promo/img/crwlogo-pc.png',
-        'img/ruud_thumbnail.jpg' : 'static/promo/img/ruud_thumbnail.jpg',
-        'img/nikita_thumbnail.jpg' : 'static/promo/img/nikita_thumbnail.jpg',
-        'img/marien_thumbnail.jpg' : 'static/promo/img/marien_thumbnail.jpg',
-        'img/justin_thumbnail.jpg' : 'static/promo/img/justin_thumbnail.jpg',
-        'img/lotte_thumbnail.jpg' : 'static/promo/img/lotte_thumbnail.jpg',
-        'img/luuk_thumbnail.jpg' : 'static/promo/img/luuk_thumbnail.jpg',
-        'img/background_row_blue.jpg' : 'static/promo/img/background_row_blue.jpg',
-        'img/banner_coffee_blue.png' : 'static/promo/img/banner_coffee_blue.png'
+        '': 'promo/index.html',
     }
     server_version = "crw/{}".format(VERSION)
 
@@ -69,35 +51,26 @@ class FileServer(BaseHTTPRequestHandler):
         write indicates if the file should actually be sent,
         or only the headers
         """
-        fname = self.resolve_filename(fname)
-        if not fname.startswith('static/'):
-            self.send_response(403)  # Forbidden
+        fname = 'static/' + self.resolve_filename(fname)
+        try:
+            mime = guess_type(fname)[0] or 'text/plain'
+            f = open(fname, 'rb')
+            self.send_response(200)  # OK
+            self.send_header('Content-type', mime)
+            self.end_headers()
+
+            if write:
+                self.wfile.write(f.read())
+
+            f.close()
+        except IOError, e:
+            self.send_response(404 if e.errno == errno.ENOENT else 403)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
 
             if write:
-                self.wfile.write('Forbidden')
-        else:
-            try:
-                mime = guess_type(fname)[0] or 'text/plain'
-                f = open(fname, 'rb')
-
-                self.send_response(200)  # OK
-                self.send_header('Content-type', mime)
-                self.end_headers()
-
-                if write:
-                    self.wfile.write(f.read())
-
-                f.close()
-            except IOError, e:
-                self.send_response(404 if e.errno == errno.ENOENT else 403)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-
-                if write:
-                    self.wfile.write(
-                        'IOError ({})'.format(errno.errorcode[e.errno]))
+                self.wfile.write(
+                    'IOError ({})'.format(errno.errorcode[e.errno]))
 
     def do_HEAD(self):
         self.send_file(self.path, write=False)
