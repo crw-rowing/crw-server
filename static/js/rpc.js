2498c0c600b2b72d3eca5e95cbc33ce4c308b6a3
Luuk G <Luuk G>
2016-12-20 21:45:07 +0100

    Changed functions to work with new json requests

--- static/js/rpc.js
+++ static/js/rpc.js
@@ -1,8 +1,13 @@
 let i=0;
-function RPCcall(method, params, callback){
+function RPCcall(method, params, user_id, session, callback){
     i++;
-    let obj = {"jsonrpc": "2.0", "method": method, "params": params, "id": i};
-	
+    if (user_id == null && session == null) {
+        var obj = {"jsonrpc": "2.0", "method": method, "params": params, "id": i};
+    }
+    else {
+        var obj = {"jsonrpc": "2.0", "method": method, "params": params, "user_id" : user_id, "session" : session, "id": i};
+	}
+    
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if ( this.readyState == 4 && this.status == 200){
